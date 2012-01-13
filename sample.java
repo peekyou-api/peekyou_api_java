242519d437dd6bfa82bb72631e33676f629d34b6
Amauris De Jesus <amauris@peekyou.com>
2012-01-13 16:58:21 -0500

    Updated sample calls

--- sample.java
+++ sample.java
@@ -1,15 +1,17 @@
 // Sample of code using peekyou_api
-package peekyou_api;
+package peekyou;
 
 
-public class sample {
-    public static void main(String[] args) {
+public class sample{
+	public static void main(String args[]){
         
         peekyou_api test=new peekyou_api();
         test.set_key("YOUR API KEY GOES HERE");
-       //second parameter for get_url can be xml,json
-       System.out.print(test.get_url("http://twitter.com/michaelhussey","xml"));
-       
+        test.set_app_id("YOUR APP ID GOES HERE");
+        
+        //second parameter for get_url can be xml,json
+        System.out.print(test.get_social_audience_info("http://twitter.com/michaelhussey","json"));
+        System.out.print(test.get_social_consumer_info("http://twitter.com/michaelhussey","json"));
 
     }
 }
